6bd754589ebe06fba2fd7a8672dd15c03664fa9e
san211 <san211@lehigh.edu>
2014-09-01 20:40:40 +0000

    updated Welcome Classe

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,49 @@
+//Safiya Nieves
+//Homework 1
+//Welcome Class Java Program
+//Due 09.02.2014
+
+//define a class
+
+public class WelcomeClass
+{
+    
+//add main method
+
+    public static void main(String[] args)
+    {
+    
+//use print out to write text line by line. Don't forget quotes and semicolon!
+
+//scrap used to check spacing
+//  -----------
+//  | WELCOME |
+//  -----------
+//  ^  ^  ^  ^  ^  ^
+// / \/ \/ \/ \/ \/ \
+//|-E--J--K--0--0--0-|
+// \ /\ /\ /\ /\ /\ /
+//  v  v  v  v  v  v
+
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        
+// "\\" prints out "\"
+
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-S--A--N--2--1--1-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        
+//autobiographical statement
+
+        System.out.println("I'm right-handed so I can't sit to the left");
+        System.out.println("of a lefty without feeling uncomfortable.");
+        
+//end
+    }
+}
+
+//closed
