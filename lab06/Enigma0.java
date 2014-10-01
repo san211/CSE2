12da864ece2924c1b201f3ba391e4c5b9cf63044
san211 <san211@lehigh.edu>
2014-10-01 16:26:16 +0000

    lab 06

---
+++ lab06/Enigma0.java
@@ -0,0 +1,58 @@
+/*Sample of expected output:
+ *   Enter an int- 9
+ *   You entered 9
+ *   sum is 25
+ *   Again, you entered 9
+ */
+
+import java.util.Scanner;
+public class Enigma0{
+  public static void main(String arg []){
+    System.out.print("Enter an int- ");
+    Scanner scan=new Scanner(System.in);
+    
+    int k=4,p=6,q=7,r=8,n=0;
+        
+    if(scan.hasNextInt()){
+      n=scan.nextInt();
+      System.out.println("You entered "+n);
+    }
+    else{
+      n=4;
+      System.out.println("You entered "+n);
+    }
+    
+    switch(k+p+q+r){
+      case 24: 
+      case 25: System.out.println("sum is 25");
+      default:
+        System.out.println("To repeat, you entered "+n);
+    }
+  }
+}
+
+/* Error report:
+ *   Expand this comment:
+ 
+        error: variable n might not have been initialized
+        System.out.println("To repeat, you entered "+n);
+                                                     ^
+        1 error
+        
+        Compilation Error
+            LINE 22
+            Variable n was declared but not initialized in outer scope
+            Initialization in inner scope does not provide initialization in outer scope
+            
+        Correction
+            Moved variable declaration to higher line
+            This allowed me to assigned dummy value of n=0.
+            After if/else n will be assigned an actual value.
+            This will print the int entered by the user
+            
+            
+            
+        Correction: 
+
+ */
+
